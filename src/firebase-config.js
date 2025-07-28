5a9f316ca16fb1399aa85dd57470a2e8b8d31fcf
Federico Ac. <50501652+Fede-Ac@users.noreply.github.com>
2025-07-28 20:26:18 -0300

    Quitar config firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGUpVBiw49aMkjZH5v6kwXWlr0MoD3qB0",
-  authDomain: "tecnobus-1f39b.firebaseapp.com",
-  projectId: "tecnobus-1f39b",
-  storageBucket: "tecnobus-1f39b.appspot.com",
-  messagingSenderId: "806322581567",
-  appId: "1:806322581567:web:ef58cfb969636087ab888a",
-  measurementId: "G-QFLPRQ9PGJ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Inicializar Firebase
